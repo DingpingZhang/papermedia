c9e8da7a4edf09b1e289ec2633e55aad4e1589c3
Dingping Zhang <dingpingzhang@outlook.com>
2017-04-09 16:43:28 +0800

    add peopledaily pipeline

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from scrapy import cmdline
 
 
-cmdline.execute("scrapy crawl doubanmovies".split())
+#cmdline.execute("scrapy crawl doubanmovies".split())
+cmdline.execute("scrapy crawl peopledaily".split())
--- papermedia/pipelines.py
+++ papermedia/pipelines.py
@@ -7,8 +7,9 @@
 
 from scrapy.conf import settings
 from papermedia.items import DoubanMovieItem
-import pymongo
+from re import RegexFlag
 import re
+import pymongo
 
 
 class DoubanMoviePipeline(object):
@@ -27,14 +28,8 @@ class DoubanMoviePipeline(object):
 
 
 class PapermediaPipeline(object):
-    def __init__(self):
-        self.__re_extract_news_info = re.compile(u'(\d{4})年(\d\d?)月(\d\d?)日[\r\n ]*?(\d\d?)[\r\n ]*?')
-
     def process_item(self, item, spider):
-        temp = self.__re_extract_news_info.match(item['news_info'])
+        temp = list(map(lambda element: element.strip(), item['news_info'].split('\r\n')))
         if temp:
-            item['news_info'] = '{}-{}-{}-layout:{}'.format(temp.group(1),
-                                                            temp.group(2),
-                                                            temp.group(3),
-                                                            temp.group(4))
+            item['news_info'] = temp[3] + temp[5] + '版'
         return item
